daedf2907a3620e899e2bb4e82f41ce4b60761a0
Avin Shum <skylighter@gmail.com>
2019-12-19 10:35:49 +0800

    lesson 53 - Running an Analysis

---
+++ knn-tf/index.js
@@ -0,0 +1,36 @@
+require('@tensorflow/tfjs-node');
+const tf = require('@tensorflow/tfjs');
+const loadCSV = require('./load-csv');
+
+function knn(features, labels, predictionPoint, k) {
+  return (
+    features
+      .sub(predictionPoint)
+      .pow(2)
+      .sum(1)
+      .pow(0.5)
+      .expandDims(1)
+      .concat(labels, 1)
+      .unstack()
+      .sort((a, b) => a.arraySync()[0] - b.arraySync()[0])
+      .slice(0, k)
+      .reduce((acc, pair) => acc + pair.arraySync()[1], 0) / k
+  );
+}
+
+let { features, labels, testFeatures, testLabels } = loadCSV(
+  'kc_house_data.csv',
+  {
+    shuffle: true,
+    splitTest: 10,
+    dataColumns: ['lat', 'long'],
+    labelColumns: ['price'],
+  },
+);
+
+features = tf.tensor(features);
+labels = tf.tensor(labels);
+
+const result = knn(features, labels, tf.tensor(testFeatures[0]), 10);
+
+console.log('Guess ', result, testLabels[0][0]);
