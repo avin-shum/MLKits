1a508657e961784f247255b1deef98c3ad79c99e
Avin Shum <skylighter@gmail.com>
2020-01-09 15:50:49 +0800

    lesson 123 - A Touch More Refactoring

---
+++ regressions/logistic-regression/logistic-regression.js
@@ -0,0 +1,121 @@
+const tf = require('@tensorflow/tfjs');
+
+class LogisticRegression {
+  constructor(features, labels, options) {
+    // For standardization
+    const { mean, variance } = tf.moments(features, 0);
+    this.mean = mean;
+    this.variance = variance;
+
+    this.features = this.processFeatures(features);
+    this.labels = tf.tensor(labels);
+    this.mseHistory = [];
+    this.bHistory = [];
+
+    this.options = Object.assign(
+      { learningRate: 0.1, iterations: 1000 },
+      options,
+    );
+
+    this.weights = tf.zeros([this.features.shape[1], 1]);
+  }
+
+  gradientDescent(features, labels) {
+    const currentGuesses = features.matMul(this.weights).sigmoid();
+    const differences = currentGuesses.sub(labels);
+
+    const slopes = features
+      .transpose()
+      .matMul(differences)
+      .div(features.shape[0]);
+
+    this.weights = this.weights.sub(slopes.mul(this.options.learningRate));
+  }
+
+  train() {
+    const batchQuantity = Math.floor(
+      this.features.shape[0] / this.options.batchSize,
+    );
+
+    for (let i = 0; i < this.options.iterations; ++i) {
+      this.bHistory.push(this.weights.arraySync()[0][0]);
+      for (let j = 0; j < batchQuantity; ++j) {
+        const { batchSize } = this.options;
+        const startIndex = j * batchSize;
+        const featureSlice = this.features.slice(
+          [startIndex, 0],
+          [batchSize, -1],
+        );
+        const labelSlice = this.labels.slice([startIndex, 0], [batchSize, -1]);
+
+        this.gradientDescent(featureSlice, labelSlice);
+      }
+      this.recordMSE();
+      this.updateLearningRate();
+    }
+  }
+
+  predict(observations) {
+    return this.processFeatures(observations)
+      .matMul(this.weights)
+      .sigmoid();
+  }
+
+  test(testFeatures, testLabels) {
+    testFeatures = this.processFeatures(testFeatures);
+    testLabels = tf.tensor(testLabels);
+
+    const predictions = testFeatures.matMul(this.weights);
+
+    const res = testLabels
+      .sub(predictions)
+      .pow(2)
+      .sum()
+      .arraySync();
+
+    const tot = testLabels
+      .sub(testLabels.mean())
+      .pow(2)
+      .sum()
+      .arraySync();
+
+    return 1 - res / tot;
+  }
+
+  processFeatures(features) {
+    features = tf.tensor(features);
+    features = this.standardize(features);
+    features = tf.ones([features.shape[0], 1]).concat(features, 1);
+    return features;
+  }
+
+  standardize(features) {
+    return features.sub(this.mean).div(this.variance.pow(0.5));
+  }
+
+  recordMSE() {
+    const mse = this.features
+      .matMul(this.weights)
+      .sub(this.labels)
+      .pow(2)
+      .sum()
+      .div(this.features.shape[0])
+      .arraySync();
+
+    this.mseHistory.unshift(mse);
+  }
+
+  updateLearningRate() {
+    if (this.mseHistory.length < 2) {
+      return;
+    }
+
+    if (this.mseHistory[0] > this.mseHistory[1]) {
+      this.options.learningRate /= 2;
+    } else {
+      this.options.learningRate *= 1.05;
+    }
+  }
+}
+
+module.exports = LogisticRegression;
