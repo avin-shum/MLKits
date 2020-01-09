39314146695c78ad802b3db9729fe2c2311bf2b0
Avin Shum <skylighter@gmail.com>
2020-01-09 15:14:02 +0800

    lesson 120 - Encoding Label Values

--- regressions/logistic-regression/index.js
+++ regressions/logistic-regression/index.js
@@ -9,5 +9,12 @@ const { features, labels, testFeatures, testLabels } = loadCSV(
     labelColumns: ['passedemissions'],
     shuffle: true,
     splitTest: 50,
+    converters: {
+      passedemissions: value => {
+        return value === 'TRUE' ? 1 : 0;
+      },
+    },
   },
 );
+
+console.log(labels);
