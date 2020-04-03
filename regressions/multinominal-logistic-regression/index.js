050eb277071f9b4d9ae5a1b75beeb19b0eab3869
Avin Shum <skylighter@gmail.com>
2020-04-03 16:48:02 +0800

    lesson 148 - Encoding Label Values

--- regressions/multinominal-logistic-regression/index.js
+++ regressions/multinominal-logistic-regression/index.js
@@ -6,5 +6,10 @@ const mnist = require('mnist-data');
 const mnistData = mnist.training(0, 10);
 
 const features = mnistData.images.values.map((image) => _.flatMap(image));
+const encodedLabels = mnistData.labels.values.map((label) => {
+  const row = new Array(10).fill(0);
+  row[label] = 1;
+  return row;
+});
 
-console.log(features);
+console.log(encodedLabels);
