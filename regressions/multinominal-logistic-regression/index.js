d6dad62b747feb35aedb9bc4cc0cfeab865c084f
Avin Shum <skylighter@gmail.com>
2020-04-03 16:07:00 +0800

    lesson 147 - Flattening Image Data

--- regressions/multinominal-logistic-regression/index.js
+++ regressions/multinominal-logistic-regression/index.js
@@ -1,43 +1,10 @@
 // require('@tensorflow/tfjs-node');
-const tf = require('@tensorflow/tfjs');
-const loadCSV = require('../load-csv');
 const LogisticRegression = require('./logistic-regression');
-const plot = require('node-remote-plot');
 const _ = require('lodash');
+const mnist = require('mnist-data');
 
-const { features, labels, testFeatures, testLabels } = loadCSV(
-  '../data/cars.csv',
-  {
-    dataColumns: ['horsepower', 'displacement', 'weight'],
-    labelColumns: ['mpg'],
-    shuffle: true,
-    splitTest: 50,
-    converters: {
-      mpg: value => {
-        const mpg = parseFloat(value);
+const mnistData = mnist.training(0, 10);
 
-        if (mpg < 15) {
-          return [1, 0, 0];
-        } else if (mpg < 30) {
-          return [0, 1, 0];
-        } else {
-          return [0, 0, 1];
-        }
-      },
-    },
-  },
-);
+const features = mnistData.images.values.map((image) => _.flatMap(image));
 
-const regression = new LogisticRegression(features, _.flatMap(labels), {
-  learningRate: 0.5,
-  iterations: 100,
-  batchSize: 50,
-  decisionBoundary: 0.5,
-});
-
-regression.train();
-console.log(regression.test(testFeatures, _.flatMap(testLabels)));
-
-// plot({
-//   x: regression.costHistory.reverse(),
-// });
+console.log(features);
