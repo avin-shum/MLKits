a61675a8a5113d1d94511165617217694ed91186
Avin Shum <skylighter@gmail.com>
2020-04-03 18:09:28 +0800

    lesson 149 - Implementing an Accuracy Gauge

--- regressions/multinominal-logistic-regression/index.js
+++ regressions/multinominal-logistic-regression/index.js
@@ -3,7 +3,7 @@ const LogisticRegression = require('./logistic-regression');
 const _ = require('lodash');
 const mnist = require('mnist-data');
 
-const mnistData = mnist.training(0, 10);
+const mnistData = mnist.training(0, 1000);
 
 const features = mnistData.images.values.map((image) => _.flatMap(image));
 const encodedLabels = mnistData.labels.values.map((label) => {
@@ -12,4 +12,22 @@ const encodedLabels = mnistData.labels.values.map((label) => {
   return row;
 });
 
-console.log(encodedLabels);
+const regression = new LogisticRegression(features, encodedLabels, {
+  learningRate: 1,
+  iterations: 5,
+  batchSize: 100,
+});
+
+regression.train();
+
+const testMnistData = mnist.testing(0, 100);
+const testFeatures = testMnistData.images.values.map((image) =>
+  _.flatMap(image),
+);
+const testEncodedLabels = testMnistData.labels.values.map((label) => {
+  const row = new Array(10).fill(0);
+  row[label] = 1;
+  return row;
+});
+const accuracy = regression.test(testFeatures, testEncodedLabels);
+console.log(`Accuracy is ${accuracy}`);
