a3e3d086bf2446cbf471f308c6ae9e0f73733b24
Avin Shum <skylighter@gmail.com>
2020-04-06 16:35:09 +0800

    lesson 160 - Releasing References

--- regressions/multinominal-logistic-regression/index.js
+++ regressions/multinominal-logistic-regression/index.js
@@ -3,16 +3,22 @@ const LogisticRegression = require('./logistic-regression');
 const _ = require('lodash');
 const mnist = require('mnist-data');
 
-const mnistData = mnist.training(0, 60000);
+const loadData = () => {
+  const mnistData = mnist.training(0, 60000);
 
-const features = mnistData.images.values.map((image) => _.flatMap(image));
-const encodedLabels = mnistData.labels.values.map((label) => {
-  const row = new Array(10).fill(0);
-  row[label] = 1;
-  return row;
-});
+  const features = mnistData.images.values.map((image) => _.flatMap(image));
+  const encodedLabels = mnistData.labels.values.map((label) => {
+    const row = new Array(10).fill(0);
+    row[label] = 1;
+    return row;
+  });
 
-const regression = new LogisticRegression(features, encodedLabels, {
+  return { features, labels: encodedLabels };
+};
+
+const { features, labels } = loadData();
+
+const regression = new LogisticRegression(features, labels, {
   learningRate: 1,
   iterations: 20,
   batchSize: 100,
