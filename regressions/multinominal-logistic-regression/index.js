e4e3eb8da36962d0cce0edc82984b625f1b39ec5
Avin Shum <skylighter@gmail.com>
2020-04-06 17:09:57 +0800

    lesson 154 - Handling Large Datasets

--- regressions/multinominal-logistic-regression/index.js
+++ regressions/multinominal-logistic-regression/index.js
@@ -3,7 +3,7 @@ const LogisticRegression = require('./logistic-regression');
 const _ = require('lodash');
 const mnist = require('mnist-data');
 
-const mnistData = mnist.training(0, 5000);
+const mnistData = mnist.training(0, 60000);
 
 const features = mnistData.images.values.map((image) => _.flatMap(image));
 const encodedLabels = mnistData.labels.values.map((label) => {
@@ -20,7 +20,7 @@ const regression = new LogisticRegression(features, encodedLabels, {
 
 regression.train();
 
-const testMnistData = mnist.testing(0, 100);
+const testMnistData = mnist.testing(0, 1000);
 const testFeatures = testMnistData.images.values.map((image) =>
   _.flatMap(image),
 );
