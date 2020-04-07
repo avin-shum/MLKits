732501c2522f47a001ac4c0ca73615215de67dd9
Avin Shum <skylighter@gmail.com>
2020-04-07 10:35:03 +0800

    lesson 170 - Plotting Cost History

--- regressions/multinominal-logistic-regression/index.js
+++ regressions/multinominal-logistic-regression/index.js
@@ -1,10 +1,11 @@
 // require('@tensorflow/tfjs-node');
 const LogisticRegression = require('./logistic-regression');
+const plot = require('node-remote-plot');
 const _ = require('lodash');
 const mnist = require('mnist-data');
 
 const loadData = () => {
-  const mnistData = mnist.training(0, 60000);
+  const mnistData = mnist.training(0, 10000);
 
   const features = mnistData.images.values.map((image) => _.flatMap(image));
   const encodedLabels = mnistData.labels.values.map((label) => {
@@ -27,7 +28,6 @@ const regression = (() => {
 })();
 
 regression.train();
-debugger;
 
 const testMnistData = mnist.testing(0, 1000);
 const testFeatures = testMnistData.images.values.map((image) =>
@@ -40,3 +40,8 @@ const testEncodedLabels = testMnistData.labels.values.map((label) => {
 });
 const accuracy = regression.test(testFeatures, testEncodedLabels);
 console.log(`Accuracy is ${accuracy}`);
+
+plot({
+  x: regression.costHistory.reverse(),
+});
+console.log(regression.costHistory);
